61c33945026e092fa1f4907a9bbc0430a3af42b6
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2023-10-04 17:29:19 -0700

    Update blackmovegenerator.cpp

--- blackmovegenerator.cpp
+++ blackmovegenerator.cpp
@@ -10,240 +10,190 @@
 
 namespace Chess {
 
-BlackMoveGenerator::BlackMoveGenerator(bool generate_enpassant_) : generate_enpassant(generate_enpassant_) {
+	BlackMoveGenerator::BlackMoveGenerator(bool generate_enpassant_) : generate_enpassant(generate_enpassant_) {
 
-	move_count = 0;
-	init_masks();
-	if (checkmask == 0) checkmask = MAX_LONG;
-	legal_squares = checkmask & ~Board::black_pieces;
+		move_count = 0;
+		init();
+		if (checkmask == 0) checkmask = MAX_LONG;
+		legal_squares = checkmask & ~Board::black_pieces;
 
-}
-
-void BlackMoveGenerator::generate_moves() {
-
-	if (in_double_check) {
-		generate_BK();
-		return;
 	}
-	generate();
-	gen_castles();
-	if (generate_enpassant) gen_ep();
-	if (Board::bitboards[BLACK_PAWN] & second_rank) gen_promotions();
 
-}
+	void BlackMoveGenerator::generate_moves() {
 
-void BlackMoveGenerator::init_masks() {
+		if (in_double_check) {
+			generate_BK();
+			return;
+		}
+		generate();
+		gen_castles();
+		if (generate_enpassant) gen_ep();
+		if (Board::bitboards[BLACK_PAWN] & second_rank) gen_promotions();
 
-	pinV = 0;
-	pinFD = 0;
-	pinH = 0;
-	pinBD = 0;
-	checkmask = 0;
+	}
 
-	int kingsq = tzcnt(Board::bitboards[BLACK_KING]);
-	checkmask |= Lookup::knight_masks[kingsq] & Board::bitboards[WHITE_KNIGHT];
-	checkmask |= Lookup::white_pawn_checkers[kingsq] & Board::bitboards[WHITE_PAWN];
-	BishopQueen = Board::bitboards[BLACK_BISHOP] | Board::bitboards[BLACK_QUEEN];
-	RookQueen = Board::bitboards[BLACK_ROOK] | Board::bitboards[BLACK_QUEEN];
+	void BlackMoveGenerator::init() {
 
-	occupied = Board::black_pieces | Board::white_pieces;
-	empty = ~occupied;
-	seen_by_enemy = squares_controlled_by_white();
-
-	uint64_t checkers = 
-	(BISHOP_ATTACKS(kingsq, Board::white_pieces) & (Board::bitboards[WHITE_QUEEN] | Board::bitboards[WHITE_BISHOP]))
-	| (ROOK_ATTACKS(kingsq, Board::white_pieces) & (Board::bitboards[WHITE_QUEEN] | Board::bitboards[WHITE_ROOK]));
+		pinV = 0;
+		pinFD = 0;
+		pinH = 0;
+		pinBD = 0;
+		checkmask = 0;
+		friendly_ksq = tzcnt(Board::bitboards[BLACK_KING]);
+		checkmask |= Lookup::knight_masks[friendly_ksq] & Board::bitboards[WHITE_KNIGHT];
+		checkmask |= Lookup::white_pawn_checkers[friendly_ksq] & Board::bitboards[WHITE_PAWN];
+		BishopQueen = Board::bitboards[BLACK_BISHOP] | Board::bitboards[BLACK_QUEEN];
+		RookQueen = Board::bitboards[BLACK_ROOK] | Board::bitboards[BLACK_QUEEN];
+		occupied = Board::black_pieces | Board::white_pieces;
+		empty = ~occupied;
+		seen_by_enemy = squares_controlled_by_white();
+		uint64_t checkers = 
+		(BISHOP_ATTACKS(friendly_ksq, Board::white_pieces) & (Board::bitboards[WHITE_QUEEN] | Board::bitboards[WHITE_BISHOP]))
+		| (ROOK_ATTACKS(friendly_ksq, Board::white_pieces) & (Board::bitboards[WHITE_QUEEN] | Board::bitboards[WHITE_ROOK]));
 	
-	for (;checkers; checkers = blsr(checkers)) {
-		uint64_t checkray = Lookup::checkmask[kingsq][tzcnt(checkers)];
-		uint64_t crossfire_blacks = Board::black_pieces & checkray;
-		checkmask |= checkray * (crossfire_blacks == 0);
-		uint64_t pinray = checkray * (popcnt(crossfire_blacks) == 1);
-		pinV  |= pinray & Lookup::vertical[kingsq];
-		pinFD |= pinray & Lookup::forward_diagonal[kingsq];
-		pinH  |= pinray & Lookup::horizontal[kingsq];
-		pinBD |= pinray & Lookup::back_diagonal[kingsq];
+		for (;checkers; checkers = blsr(checkers)) {
+			uint64_t checkray = Lookup::checkmask[friendly_ksq][tzcnt(checkers)];
+			uint64_t crossfire_blacks = Board::black_pieces & checkray;
+			checkmask |= checkray * (crossfire_blacks == 0);
+			uint64_t pinray = checkray * (popcnt(crossfire_blacks) == 1);
+			pinV  |= pinray & Lookup::vertical[friendly_ksq];
+			pinFD |= pinray & Lookup::forward_diagonal[friendly_ksq];
+			pinH  |= pinray & Lookup::horizontal[friendly_ksq];
+			pinBD |= pinray & Lookup::back_diagonal[friendly_ksq];
+		}
+
+		pin_O = pinV | pinH;
+		pin_D = pinFD | pinBD;
+		not_pinned = ~(pin_O | pin_D);
+		in_double_check = (popcnt(Lookup::double_check[friendly_ksq] & checkmask) > 1);
+
 	}
 
-	pin_O = pinV | pinH;
-	pin_D = pinFD | pinBD;
-	not_pinned = ~(pin_O | pin_D);
-	in_double_check = (popcnt(Lookup::double_check[kingsq] & checkmask) > 1);
+	void BlackMoveGenerator::generate() {
 
-}
+		uint64_t pawns = Board::bitboards[BLACK_PAWN] & not_2_or_a & ~(pin_O | pinBD);
+		for (uint64_t downleft = (pawns >> 7) & Board::white_pieces & checkmask; downleft; downleft = blsr(downleft)) {
+			int to = tzcnt(downleft);
+			moves[move_count++] = to + 7 + (to << 6);
+		}
+		pawns = Board::bitboards[BLACK_PAWN] & not_2_or_h & ~(pin_O | pinFD);
+		for (uint64_t downright = (pawns >> 9) & Board::white_pieces & checkmask; downright; downright = blsr(downright)) {
+			int to = tzcnt(downright);
+			moves[move_count++] = to + 9 + (to << 6);
+		}
+		pawns = Board::bitboards[BLACK_PAWN] & ~(pin_D | pinH | second_rank);
+		for (uint64_t push = (pawns >> 8) & empty & checkmask; push; push = blsr(push)) {
+			int to = tzcnt(push);
+			moves[move_count++] = to + 8 + (to << 6);
+		}
+		uint64_t r = ((sixth_rank & empty) >> 8) & empty;
+		pawns = Board::bitboards[BLACK_PAWN] & ~(pin_D | pinH);
+		for (uint64_t double_push = (pawns >> 16) & r & checkmask; double_push; double_push = blsr(double_push)) {
+			int to = tzcnt(double_push);
+			moves[move_count++] = to + 16 + (to << 6);
+		}
+		for (uint64_t from_map = Board::bitboards[BLACK_KNIGHT] & not_pinned; from_map; from_map = blsr(from_map)) {
+			int from = tzcnt(from_map);
+			for (uint64_t to = Lookup::knight_masks[from] & legal_squares; to; to = blsr(to))
+				moves[move_count++] = from + (tzcnt(to) << 6);
+		}
+		for (uint64_t from_map = BishopQueen & not_pinned; from_map; from_map = blsr(from_map)) {
+			int from = tzcnt(from_map);
+			for (uint64_t to = BISHOP_ATTACKS(from, occupied) & legal_squares; to; to = blsr(to))
+				moves[move_count++] = from + (tzcnt(to) << 6);
+		}
+		for (uint64_t from_map = BishopQueen & ~not_pinned; from_map; from_map = blsr(from_map)) {
+			int from = tzcnt(from_map);
+			for (uint64_t to = BISHOP_ATTACKS(from, occupied) & legal_squares & Lookup::pinmask[friendly_ksq][from]; to; to = blsr(to))
+				moves[move_count++] = from + (tzcnt(to) << 6);
+		}
+		for (uint64_t from_map = RookQueen & not_pinned; from_map; from_map = blsr(from_map)) {
+			int from = tzcnt(from_map);
+			for (uint64_t to = ROOK_ATTACKS(from, occupied) & legal_squares; to; to = blsr(to))
+				moves[move_count++] = from + (tzcnt(to) << 6);
+		}
+		for (uint64_t from_map = RookQueen & ~not_pinned; from_map; from_map = blsr(from_map)) {
+			int from = tzcnt(from_map);
+			for (uint64_t to = ROOK_ATTACKS(from, occupied) & legal_squares & Lookup::pinmask[friendly_ksq][from]; to; to = blsr(to))
+				moves[move_count++] = from + (tzcnt(to) << 6);
+		}
+		for (uint64_t to = Lookup::king_masks[friendly_ksq] & ~(Board::black_pieces | seen_by_enemy); to; to = blsr(to))
+			moves[move_count++] = friendly_ksq + (tzcnt(to) << 6);
 
-void BlackMoveGenerator::generate() {
-
-	uint64_t piece_map = Board::bitboards[BLACK_PAWN] & not_2_or_a & ~(pin_O | pinBD);
-	uint64_t move_map = (piece_map >> 7) & Board::white_pieces & checkmask;
-	int move_square;
-	for (;move_map; move_map = blsr(move_map)) {
-		move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 7 + (move_square << 6);
-	}
-	piece_map = Board::bitboards[BLACK_PAWN] & not_2_or_h & ~(pin_O | pinFD);
-	move_map = (piece_map >> 9) & Board::white_pieces & checkmask;
-	for (;move_map; move_map = blsr(move_map)) {
-		move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 9 + (move_square << 6);
-	}
-	piece_map = Board::bitboards[BLACK_PAWN] & ~(pin_D | pinH | second_rank);
-	move_map = (piece_map >> 8) & empty & checkmask;
-	for (;move_map; move_map = blsr(move_map)) {
-		move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 8 + (move_square << 6);
-	}
-	move_map = (piece_map >> 8) & empty;
-	move_map = (move_map >> 8) & empty & fifth_rank & checkmask;
-	for (;move_map; move_map = blsr(move_map)) {
-		move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 16 + (move_square << 6);
-	}
-	piece_map = Board::bitboards[BLACK_KNIGHT] & not_pinned;
-	int piece_square;
-	for (;piece_map; piece_map = blsr(piece_map)) {
-		piece_square = tzcnt(piece_map);
-		move_map = Lookup::knight_masks[piece_square] & legal_squares;
-		for (;move_map; move_map = blsr(move_map))
-			moves[move_count++] = piece_square + (tzcnt(move_map) << 6);
-	}
-	piece_map = BishopQueen & not_pinned;
-	for (;piece_map; piece_map = blsr(piece_map)) {
-		piece_square = tzcnt(piece_map);
-		move_map = BISHOP_ATTACKS(piece_square, occupied) & legal_squares;
-		for (;move_map; move_map = blsr(move_map))
-			moves[move_count++] = piece_square + (tzcnt(move_map) << 6);
-	}
-	piece_map = BishopQueen & pinFD;
-	for (;piece_map; piece_map = blsr(piece_map)) {
-		piece_square = tzcnt(piece_map);
-		move_map = BISHOP_ATTACKS(piece_square, occupied) & legal_squares & pinFD;
-		for (;move_map; move_map = blsr(move_map))
-			moves[move_count++] = piece_square + (tzcnt(move_map) << 6);
-	}
-	piece_map = BishopQueen & pinBD;
-	for (;piece_map; piece_map = blsr(piece_map)) {
-		piece_square = tzcnt(piece_map);
-		move_map = BISHOP_ATTACKS(piece_square, occupied) & legal_squares & pinBD;
-		for (;move_map; move_map = blsr(move_map))
-			moves[move_count++] = piece_square + (tzcnt(move_map) << 6);
-	}
-	piece_map = RookQueen & not_pinned;
-	for (;piece_map; piece_map = blsr(piece_map)) {
-		piece_square = tzcnt(piece_map);
-		move_map = ROOK_ATTACKS(piece_square, occupied) & legal_squares;
-		for (;move_map; move_map = blsr(move_map))
-			moves[move_count++] = piece_square + (tzcnt(move_map) << 6);
-	}
-	piece_map = RookQueen & pinV;
-	for (;piece_map; piece_map = blsr(piece_map)) {
-		piece_square = tzcnt(piece_map);
-		move_map = ROOK_ATTACKS(piece_square, occupied) & legal_squares & pinV;
-		for (;move_map; move_map = blsr(move_map))
-			moves[move_count++] = piece_square + (tzcnt(move_map) << 6);
-	}
-	piece_map = RookQueen & pinH;
-	for (;piece_map; piece_map = blsr(piece_map)) {
-		piece_square = tzcnt(piece_map);
-		move_map = ROOK_ATTACKS(piece_square, occupied) & legal_squares & pinH;
-		for (;move_map; move_map = blsr(move_map))
-			moves[move_count++] = piece_square + (tzcnt(move_map) << 6);
-	}
-	piece_square = tzcnt(Board::bitboards[BLACK_KING]);
-	move_map = Lookup::king_masks[piece_square] & ~(Board::black_pieces | seen_by_enemy);
-	for (;move_map; move_map = blsr(move_map))
-		moves[move_count++] = piece_square + (tzcnt(move_map) << 6);
-
-}
-
-void BlackMoveGenerator::gen_promotions() {
-
-	uint64_t promotable = Board::bitboards[BLACK_PAWN] & second_rank;
-	uint64_t piece_map = promotable & ~(afile | pin_O | pinBD);
-	uint64_t move_map = (piece_map >> 7) & Board::white_pieces & checkmask;
-	int move_square;
-	for (; move_map; move_map = blsr(move_map)) {
-		move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 7 + (move_square << 6) + promotion_flag;
-	}
-	piece_map = promotable & ~(hfile | pin_O | pinFD);
-	move_map = (piece_map >> 9) & Board::white_pieces & checkmask;
-	for (; move_map; move_map = blsr(move_map)) {
-		move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 9 + (move_square << 6) + promotion_flag;
-	}
-	piece_map = promotable & not_pinned;
-	move_map = (piece_map >> 8) & empty & checkmask;
-	for (; move_map; move_map = blsr(move_map)) {
-		move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 8 + (move_square << 6) + promotion_flag;
 	}
 
-}
+	void BlackMoveGenerator::gen_promotions() {
 
-void BlackMoveGenerator::gen_ep() {
+		uint64_t pawns_on_2 = Board::bitboards[BLACK_PAWN] & second_rank;
+		uint64_t pawns = pawns_on_2 & ~(afile | pin_O | pinBD);
+		for (uint64_t downleft = (pawns >> 7) & Board::white_pieces & checkmask; downleft; downleft = blsr(downleft)) {
+			int to = tzcnt(downleft);
+			moves[move_count++] = to + 7 + (to << 6) + promotion_flag;
+		}
+		pawns = pawns_on_2 & ~(hfile | pin_O | pinFD);
+		for (uint64_t downright = (pawns >> 9) & Board::white_pieces & checkmask; downright; downright = blsr(downright)) {
+			int to = tzcnt(downright);
+			moves[move_count++] = to + 9 + (to << 6) + promotion_flag;
+		}
+		pawns = pawns_on_2 & not_pinned;
+		for (uint64_t push = (pawns >> 8) & empty & checkmask; push; push = blsr(push)) {
+			int to = tzcnt(push);
+			moves[move_count++] = to + 8 + (to << 6) + promotion_flag;
+		}
 
-	uint64_t move_map = (Board::bitboards[BLACK_PAWN] >> 7) & GameState::current_ep_square() & third_rank;
-	if (move_map && !((Board::bitboards[BLACK_KING] & third_rank) && ((Board::bitboards[WHITE_QUEEN] | Board::bitboards[WHITE_ROOK]) & third_rank))) { // <- not perfect, but ensures king cant be captured
-		int move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 7 + (move_square << 6) + ep_flag;
 	}
-	move_map = (Board::bitboards[BLACK_PAWN] >> 9) & GameState::current_ep_square() & third_rank;
-	if (move_map && !((Board::bitboards[BLACK_KING] & third_rank) && ((Board::bitboards[WHITE_QUEEN] | Board::bitboards[WHITE_ROOK]) & third_rank))) {
-		int move_square = tzcnt(move_map);
-		moves[move_count++] = move_square + 9 + (move_square << 6) + ep_flag;
+
+	void BlackMoveGenerator::gen_ep() {
+
+		uint64_t move_map = (Board::bitboards[BLACK_PAWN] >> 7) & GameState::current_ep_square() & third_rank;
+		if (move_map && !((Board::bitboards[BLACK_KING] & third_rank) && ((Board::bitboards[WHITE_QUEEN] | Board::bitboards[WHITE_ROOK]) & third_rank))) { // <- not perfect, but ensures king cant be captured
+			int move_square = tzcnt(move_map);
+			moves[move_count++] = move_square + 7 + (move_square << 6) + ep_flag;
+		}
+		move_map = (Board::bitboards[BLACK_PAWN] >> 9) & GameState::current_ep_square() & third_rank;
+		if (move_map && !((Board::bitboards[BLACK_KING] & third_rank) && ((Board::bitboards[WHITE_QUEEN] | Board::bitboards[WHITE_ROOK]) & third_rank))) {
+			int move_square = tzcnt(move_map);
+			moves[move_count++] = move_square + 9 + (move_square << 6) + ep_flag;
+		}
+
 	}
 
-}
+	void BlackMoveGenerator::gen_castles() {
 
-void BlackMoveGenerator::gen_castles() {
+		if (~checkmask) return;
+		if ((!(occupied & 0x600000000000000ull)) && !is_attacked(0x600000000000000ull) && GameState::rights_k())
+			moves[move_count++] = scastle_flag;
+		if ((!(occupied & 0x7000000000000000ull)) && !is_attacked(0x3000000000000000ull) && GameState::rights_q())
+			moves[move_count++] = lcastle_flag;
 
-	if (~checkmask) return;
-	if ((!(occupied & 0x600000000000000ull)) && !is_attacked(0x600000000000000ull) && GameState::rights_k())
-		moves[move_count++] = scastle_flag;
-	if ((!(occupied & 0x7000000000000000ull)) && !is_attacked(0x3000000000000000ull) && GameState::rights_q())
-		moves[move_count++] = lcastle_flag;
+	}
 
-}
+	void BlackMoveGenerator::generate_BK() {
 
-void BlackMoveGenerator::generate_BK() {
+		for (uint64_t to = Lookup::king_masks[friendly_ksq] & ~(Board::black_pieces | seen_by_enemy); to; to = blsr(to))
+			moves[move_count++] = friendly_ksq + (tzcnt(to) << 6);
 
-	int kingsq = tzcnt(Board::bitboards[BLACK_KING]);
-	uint64_t move_map = Lookup::king_masks[kingsq] & ~(Board::black_pieces | seen_by_enemy);
-	int move_square;
-	for (; move_map; move_map = blsr(move_map))
-		moves[move_count++] = kingsq + (tzcnt(move_map) << 6);
+	}
 
-}
+	inline bool BlackMoveGenerator::is_attacked(uint64_t squares) {
 
-bool BlackMoveGenerator::is_attacked(uint64_t squares) {
+		return squares & seen_by_enemy;
 
-	return squares & seen_by_enemy;
+	}
 
-}
+	uint64_t BlackMoveGenerator::squares_controlled_by_white() {
 
-bool BlackMoveGenerator::incheck() {
+		uint64_t occ_kingless = occupied ^ (1ull << friendly_ksq);
+		uint64_t protected_squares = ((Board::bitboards[WHITE_PAWN] << 9) & NOT_FILE_H) | ((Board::bitboards[WHITE_PAWN] << 7) & NOT_FILE_A);
 
-	return ~checkmask;
+		for (uint64_t piece_map = Board::bitboards[WHITE_KNIGHT]; piece_map; piece_map = blsr(piece_map))
+			protected_squares |= Lookup::knight_masks[tzcnt(piece_map)];
+		for (uint64_t piece_map = Board::bitboards[WHITE_BISHOP] | Board::bitboards[WHITE_QUEEN]; piece_map; piece_map = blsr(piece_map))
+			protected_squares |= BISHOP_ATTACKS(tzcnt(piece_map), occ_kingless);
+		for (uint64_t piece_map = Board::bitboards[WHITE_ROOK] | Board::bitboards[WHITE_QUEEN]; piece_map; piece_map = blsr(piece_map))
+			protected_squares |= ROOK_ATTACKS(tzcnt(piece_map), occ_kingless);
+		return protected_squares | Lookup::king_masks[tzcnt(Board::bitboards[WHITE_KING])];
 
-}
-
-uint64_t BlackMoveGenerator::squares_controlled_by_white() {
-
-	uint64_t occ_kingless = occupied ^ (1ull << tzcnt(Board::bitboards[BLACK_KING]));
-	uint64_t protected_squares = ((Board::bitboards[WHITE_PAWN] << 9) & ~hfile) | ((Board::bitboards[WHITE_PAWN] << 7) & ~afile);
-
-	uint64_t piece_map = Board::bitboards[WHITE_KNIGHT];
-	for (;piece_map; piece_map = blsr(piece_map))
-		protected_squares |= Lookup::knight_masks[tzcnt(piece_map)];
-	piece_map = Board::bitboards[WHITE_BISHOP] | Board::bitboards[WHITE_QUEEN];
-	for (;piece_map; piece_map = blsr(piece_map))
-		protected_squares |= BISHOP_ATTACKS(tzcnt(piece_map), occ_kingless);
-	piece_map = Board::bitboards[WHITE_ROOK] | Board::bitboards[WHITE_QUEEN];
-	for (;piece_map; piece_map = blsr(piece_map))
-		protected_squares |= ROOK_ATTACKS(tzcnt(piece_map), occ_kingless);
-	return protected_squares | Lookup::king_masks[tzcnt(Board::bitboards[WHITE_KING])];
-
-}
+	}
 
 }
