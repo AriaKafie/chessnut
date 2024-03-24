31babef96c678715a9cec6047a94e2fa2c1fd8b1
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2024-03-24 15:35:50 -0700

    Add files via upload

--- src/magic.cpp
+++ src/magic.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <time.h>
 
+
 std::mt19937_64 rng(time(NULL));
 
 uint64_t magic_candidate();
@@ -24,23 +25,20 @@ bool has_collisions(const std::vector<int>& keys, const std::vector<uint64_t>& v
     02: 0xbe5e8d8b1d47fc56ull
 */
 
-void thing() {
+void Magic::test_magic(Square sq, uint64_t magic) {
 
   std::ofstream o("C:\\Users\\14244\\Desktop\\magic.txt");
+
   if (!o.is_open()) {
     std::cout << "file open failed";
     std::exit(0);
   }
 
-  const uint64_t magic = 0xa000a104004092ull;
-  const uint64_t mask = rook_masks[A8];
-  int bitcount = popcount(mask);
-  int permutations = 1 << bitcount;
-
-  for (int p = 0; p < permutations; p++) {
-    Bitboard occupancy = generate_occupancy(mask, p);
-    o << bbtos(occupancy) << ((occupancy * magic) >> (64 - bitcount)) << "\n";
+  for (int p = 0; p < 1 << popcount(rook_masks[sq]); p++) {
+    Bitboard occupancy = generate_occupancy(rook_masks[sq], p);
+    o << bbtos(occupancy) << ((occupancy * magic) >> (64 - popcount(rook_masks[sq]))) << "\n";
   }
+
   o.close();
   std::cout << "file write successful";
 
@@ -48,9 +46,8 @@ void thing() {
 
 void Magic::search() {
 
-  //thing(); return;
-  constexpr uint64_t search_time = 120000;
   std::cout << "Bitboard rook_magics[SQUARE_NB] =\n{\n";
+
   for (Square sq = H1; sq <= A8; sq++) {
 
     std::vector<Bitboard> occupancies;
@@ -66,18 +63,15 @@ void Magic::search() {
     }
 
     uint64_t magic;
-    bool valid_magic = false;
+    std::vector<int> keys(64);
 
-    using Util::curr_time_millis;
-    auto start = curr_time_millis();
-
-    while (!valid_magic) {
+    do
+    {
       magic = magic_candidate();
-      std::vector<int> keys;
       for (Bitboard occupancy : occupancies)
-        keys.push_back(occupancy * magic >> 64 - bitcount);
-      valid_magic = !has_collisions(keys, attacks);
-    }
+        keys.push_back((occupancy * magic) >> (64 - bitcount));
+    } while (!has_collisions(keys, attacks));
+
     std::cout << "  0x" << std::hex << magic << "ull,\n";
   }
   std::cout << "};\n";
