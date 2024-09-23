14190cd810fbbd073d1979f3a202e3f090b41b72
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2024-09-23 11:13:33 -0700

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -102,20 +102,11 @@ int search(int alpha, int beta, int depth, int ply_from_root, bool null_ok) {
 
     moves.sort(TranspositionTable::lookup_move(), ply_from_root);
 
-    int static_evaluation = NO_EVAL, extension = root_node ? 0 : moves.in_check();
+    int extension = root_node ? 0 : moves.in_check();
 
     for (int i = 0; i < moves.size(); i++)
     {
         int reduction = root_node ? root_reductions[i] : reductions[depth][i];
-
-        /*if ((i > 0) && ((depth - 1 - reduction + extension) <= 0) && type_of(moves[i]) == NORMAL && !piece_on(to_sq(moves[i])))
-        {
-            if (static_evaluation == NO_EVAL)
-                static_evaluation = static_eval<SideToMove>();
-
-            if (static_evaluation + 300 <= alpha)
-                continue;
-        }*/
 
         do_move<SideToMove>(moves[i]);
 
@@ -165,7 +156,7 @@ void iterative_deepening(int max_depth = 64) {
         if (search_cancelled)
             break;
 
-        //std::cout << "info depth " << depth << " score cp " << eval << " nodes " << nodes << " pv " << move_to_uci(root_move) << std::endl;
+        std::cout << "info depth " << depth << " score cp " << eval << " nodes " << nodes << " pv " << move_to_uci(root_move) << std::endl;
     }
 }
 
