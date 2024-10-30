ad727019402f27c3a5ff134c07af23702636bc4a
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2024-10-29 22:15:20 -0700

    Add files via upload

--- src/uci.cpp
+++ src/uci.cpp
@@ -94,9 +94,7 @@ void UCI::loop()
 
 Move uci_to_move(const std::string& uci)
 {
-    Move list[MAX_MOVES];
-
-    for (Move *m = list, *end = get_moves(list); m != end; m++)
+    for (Move list[MAX_MOVES], *m = list, *end = get_moves(list); m != end; m++)
         if (move_to_uci(*m) == uci) return *m;
 
     return NO_MOVE;
