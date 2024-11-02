293ef06e68aa06c504cc0380d045dff8f0401c1e
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2024-11-01 17:06:28 -0700

    Add files via upload

--- src/transpositiontable.h
+++ src/transpositiontable.h
@@ -12,10 +12,11 @@ enum { EXACT, UPPER_BOUND, LOWER_BOUND };
 
 constexpr int NO_EVAL = 0x7fffffff;
 
-constexpr int TT_SIZE = 1 << 24;
-constexpr int RT_SIZE = 1 << 15;
+constexpr int TT_SIZE = 1 << 24; // 256 MiB
+constexpr int RT_SIZE = 1 << 15; // 512 KiB
 
-struct Entry {
+struct Entry
+{
     uint64_t  key;
     int       eval;
     uint16_t  best_move;
@@ -23,27 +24,26 @@ struct Entry {
     BoundType flag;
 };
 
-struct RepInfo {
+struct RepInfo
+{
     uint64_t key;
     uint8_t  occurrences;
 };
 
-namespace TranspositionTable {
+namespace TranspositionTable
+{
+    int lookup(int depth, int alpha, int beta, int ply_from_root);
+    void record(uint8_t depth, BoundType flag, int eval, Move best_move, int ply_from_root);
+    Move lookup_move();
+    void clear();
+} // namespace TranspositionTable
 
-int lookup(int depth, int alpha, int beta, int ply_from_root);
-void record(uint8_t depth, BoundType flag, int eval, Move best_move, int ply_from_root);
-Move lookup_move();
-void clear();
-
-} //namespace TranspositionTable
-
-namespace RepetitionTable {
-
-bool draw();
-void push();
-void pop();
-void clear();
-
+namespace RepetitionTable
+{
+    bool draw();
+    void push();
+    void pop();
+    void clear();
 } // namespace RepetitionTable 
 
 #endif
